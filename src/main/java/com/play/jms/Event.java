1084c9b944c843b8b119cbe33c4b6fe3bd1ad5df
Illia Iegorov (et0427) <Illia.Iegorov@evry.com>
2023-05-17 16:31:37 +0300

    add lombok support

--- src/main/java/com/play/jms/Event.java
+++ src/main/java/com/play/jms/Event.java
@@ -6,32 +6,21 @@
  */
 package com.play.jms;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Event {
 
     private int id;
     private String message;
 
-    public Event() {
-    }
-
     public Event(int id, String message) {
         this.id = id;
-        this.message = message;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
